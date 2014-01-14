23e8e85b38d9df925bf33e6771230660bebde052
Jeffrey Richardson <Jrichardson@Jeffreys-MacBook-Pro.local>
2014-01-13 20:57:04 -0600

    fix production env

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ module.exports = {
 		facebook: {
 			clientID: 1377040429223933,
 			clientSecret: "7f89d8194144c43926e4ce396928a9c3",
-			callbackURL: "http://localhost:3000/auth/facebook/callback"
+			callbackURL: "http://secret-lowlands-2428.herokuapp.com/auth/facebook/callback"
 		}
  	}
 }
